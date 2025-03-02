4d5a5eed3123cfa3f62d4281929ce9085233a952
MarcusOcampo <159239180+MarcusOcampo@users.noreply.github.com>
2025-03-02 11:16:23 +0800

    docs: added documentation for dynamic handling

--- nextjs-demo/src/app/users/[id]/route.ts
+++ nextjs-demo/src/app/users/[id]/route.ts
@@ -1,8 +1,13 @@
 // dynamic route handler
 import { users } from "../route";
 
+// for finding a user based on id (user thunder client http://localhost:3000/users/2 for example to find id 2)
 export async function GET(request: Request, { params }: {params: {id: string}}) {
     const {id} = await params;
     const user = users.find((user) => user.id === parseInt(id));
     return Response.json(user);
 }
+
+// implement delete
+
+// implement update
