e57226aa8d00ad52b5861c47485bec4380e4f7be
MarcusOcampo <159239180+MarcusOcampo@users.noreply.github.com>
2025-03-02 11:15:17 +0800

    feat: route handling

---
+++ nextjs-demo/src/app/users/[id]/route.ts
@@ -0,0 +1,8 @@
+// dynamic route handler
+import { users } from "../route";
+
+export async function GET(request: Request, { params }: {params: {id: string}}) {
+    const {id} = await params;
+    const user = users.find((user) => user.id === parseInt(id));
+    return Response.json(user);
+}
--- nextjs-demo/src/app/users/route.ts
+++ nextjs-demo/src/app/users/route.ts
@@ -1,7 +1,23 @@
-export const user = [
+export const users = [
     {id: 1, name: "John Doe"},
     {id: 2, name: "Jane Doe"},
 ]
 
-export async function GET() {}
-export async function POST() {}
+export async function GET() {
+    return Response.json(users);
+}
+// add user in database
+export async function POST(request: Request) {
+    const user = await request.json()
+    const newUser = {
+        id: users.length + 1,
+        name: user.name,
+    };
+    users.push(newUser);
+    return new Response(JSON.stringify(newUser), {
+        headers: {
+            "Content-Type": "application.json",
+        },
+        status: 201,
+    });
+}
